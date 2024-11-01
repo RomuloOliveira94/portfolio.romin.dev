e741ecebe64c811bd8809026b739b8f0b95bfbed
Rômulo Oliveira <romuloffall@gmail.com>
2024-11-01 17:15:23 -0300

    #fix site meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: "https://romin.dev.br/",
+  site: "https://portfolio.romin.dev.br/",
   i18n: {
     defaultLocale: "pt-br",
     locales: ["en", "es", "pt-br"]
